ecf68546dfe23bded1a6d62f4c0f3bc5d2779f0d
AmineHLub <Amino.bhl@gmail.com>
2021-10-12 22:42:18 +0100

    Fix items positioning at index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './style.css';
 import './popup.css';
 import generatePopup from './generatePopup.js';
-
-document.querySelectorAll('.nav-link > ul > li > a')[1].addEventListener('click', generatePopup);
 import './display.css';
 
 import sendList from './displayList.js';
+
+document.querySelectorAll('.nav-link > ul > li > a')[1].addEventListener('click', generatePopup);
 
 sendList();
