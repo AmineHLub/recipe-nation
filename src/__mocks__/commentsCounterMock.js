2ace8ed2035e8342e8e0c63e3c5159c6a362bdbe
AmineHLub <Amino.bhl@gmail.com>
2021-10-14 00:56:51 +0100

    Add tests
    Update commentsCounter.js function : Add try and catch
    
    Add fetch-mock to package.json

--- src/__mocks__/commentsCounterMock.js
+++ src/__mocks__/commentsCounterMock.js
@@ -1,14 +1,14 @@
-const commentsCounter = async (mealId) => {
+const commentsCounterMock = async () => {
   try {
     const websiteID = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/5OvdDl22dcA0TUpGjcko';
-    const fetchComments = fetch(`${websiteID}/comments?item_id=${mealId}`);
+    const fetchComments = fetch(`${websiteID}/comments?item_id=${156165146}`);
     const getComments = await fetchComments;
     const commentsjson = await getComments.json();
     const commentsArr = commentsjson;
-    return commentsArr;
+    return commentsArr.length;
   } catch (e) {
     return null;
   }
 };
 
-export default commentsCounter;
+module.exports = commentsCounterMock;
