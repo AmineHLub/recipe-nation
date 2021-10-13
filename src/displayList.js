a3102b8e097d9d403e68b916332e40c864424e78
mahbub <mahbub21alam2017@gmail.com>
2021-10-13 20:28:52 +0600

    Add likes to DOM using pre-Post on likes on intervention API

--- src/displayList.js
+++ src/displayList.js
@@ -8,26 +8,42 @@ const sendList = () => {
     const mealObj = jsonObject.meals[0];
     return mealObj;
   };
+  const websiteID = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/5OvdDl22dcA0TUpGjcko';
+  const getLikes = async () => {
+    const fetchLikes = fetch(`${websiteID}/likes/`);
+    const likesCount = await fetchLikes;
+    const likesJson = await likesCount.json();
+    const { likes } = likesJson[0];
+    return likes;
+  };
 
+  //       likes.innerText = `likes : ${getLikes().then((res) => res)}`;
   for (let i = 0; i < 9; i += 1) {
     fetchData().then((res) => {
-      const items = document.createElement('div');
-      items.id = `meal${res.idMeal}`;
-      const image = document.createElement('img');
-      const heading = document.createElement('h5');
-      const commentBtn = document.createElement('button');
-      commentBtn.innerText = 'Comments';
-      document.querySelector('main').append(items);
-      items.append(image);
-      items.append(heading);
-      items.append(commentBtn);
-      image.src = `${res.strMealThumb}`;
-      heading.innerText = `${res.strMeal}`;
-      commentBtn.classList.add('c-btn');
-      items.classList.add('item');
-      image.classList.add('item-image');
-      image.addEventListener('click', generatePopup.bind(null, res.idMeal));
+      getLikes().then((likesCount) => {
+        const items = document.createElement('div');
+        items.id = `meal${res.idMeal}`;
+        const image = document.createElement('img');
+        const heading = document.createElement('h5');
+        const commentBtn = document.createElement('button');
+        const likes = document.createElement('span');
+        likes.classList.add('likes-counter');
+        likes.innerText = `likes : ${likesCount}\n`;
+        commentBtn.innerText = 'Comments';
+        document.querySelector('main').append(items);
+        items.append(image);
+        items.append(heading);
+        items.append(likes);
+        items.append(commentBtn);
+        image.src = `${res.strMealThumb}`;
+        heading.innerText = `${res.strMeal}`;
+        commentBtn.classList.add('c-btn');
+        items.classList.add('item');
+        image.classList.add('item-image');
+        image.addEventListener('click', generatePopup.bind(null, res.idMeal));
+      });
     });
   }
 };
+
 export default sendList;
